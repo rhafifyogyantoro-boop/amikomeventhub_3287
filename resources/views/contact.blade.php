cc0ecac4e38a628e0e882221b95dddef4010a9cf
Rhafif Yogiantoro <rhafifyogyantoro@gmail.com>
2026-04-10 10:19:42 +0700

    Tugas1

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -9,7 +9,17 @@
     <div class="bg-white p-8 rounded-x1 shadow-lg border border-slate-200 text-center max-w-sm w-full">
         <h1 class="text-2xl font-bold text-slate-800 mb-2">Hubungi Pihak Kami</h1>
         <p class="text-slate-500 mb-6">Email : admin@amikomeventhub.com</p>
-        <a href="/tentang" class="inline-block bg-indigo-600 text-white font-semibold py-2 px-6 rounded-lg hover:bg-indigo-700 hover:shadow-md transition duration-300">Kembali ke Home</a>
+           <div class="flex flex-wrap justify-center gap-4">
+            <a href="/profil" class="inline-block bg-red-600 text-white font-semibold py-2 px-6 rounded-lg hover:bg-red-700 hover:shadow-md transition duration-300">
+            profil
+            </a>
+            <a href="/bantuan" class="inline-block bg-yellow-600 text-white font-semibold py-2 px-6 rounded-lg hover:bg-yellow-700 hover:shadow-md transition duration-300">
+            Bantuan
+            </a>
+            <a href="/katalog" class="inline-block bg-green-600 text-white font-semibold py-2 px-6 rounded-lg hover:bg-green-700 hover:shadow-md transition duration-300">
+            Katalog
+            </a>
+            </div>
     </div>
 </body>
 </html>
