69ecd991a23cf5ce0729be34b8b7a83ff8ac5bc1
Rhafif Yogiantoro <rhafifyogyantoro@gmail.com>
2026-04-10 07:56:07 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,7 @@ Route::get('/tentang', function () {
     return '<h1>Halaman Tentang Aplikasi Event Hub</h1>';
 });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/contact', function () {
     return view('contact');
